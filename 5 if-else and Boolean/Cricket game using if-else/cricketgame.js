af8d2fbee471e67f7160928ca40fe43708c1302e
Sonu Kumar Sah <sonusah22388@gmail.com>
2025-06-30 09:54:54 +0530

    added a function file

--- 5 if-else and Boolean/Cricket game using if-else/cricketgame.js
+++ 5 if-else and Boolean/Cricket game using if-else/cricketgame.js
@@ -26,13 +26,10 @@ function ballClick() {
   randomNumber = Math.random() * 3;
   if (randomNumber > 0 && randomNumber <= 1) {
     computerChoice = 'Bat';
-    //console.log('Computer choice is Bat');
   } else if (randomNumber > 1 && randomNumber <= 2) {
     computerChoice = 'Ball';
-    //console.log('Computer choice is Ball');
   } else {
     computerChoice = 'Stump';
-    //console.log('Computer choice is Stump');
   }
   if (computerChoice === 'Ball') {
     resultMsg = `It's a tie`;
